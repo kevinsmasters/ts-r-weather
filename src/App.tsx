0f29cf224a1c8618679c07015eecba4e0330574f
kevinsmasters <gmail@kevinmasters.com>
2023-10-25 20:59:55 -0400

    add fetch api call and display options roughly

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,29 @@
-const App = () => {
+import { ChangeEvent, useState } from 'react'
+
+const App = (): JSX.Element => {
+  const [term, setTerm] = useState<string>('')
+  const [options, setOptions] = useState<[]>([])
+  const getSearchTermOptions = (value: string) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
+        process.env.REACT_APP_API_KEY
+      }`
+    )
+      .then((res) => res.json())
+      .then((data) => setOptions(data))
+  }
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim()
+    setTerm(value)
+
+    if (value === '') return
+    getSearchTermOptions(value)
+  }
+  /*
+http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+  */
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
       <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
@@ -11,9 +36,13 @@ const App = () => {
         <div className="flex mt-10 md:mt-4">
           <input
             type="text"
-            value={''}
+            value={term}
             className="px-2 py-1 rounded-1-md border-2 border-white"
+            onChange={onInputChange}
           />
+          {options.map((option: { name: string }) => (
+            <p>{option.name}</p>
+          ))}
           <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer">
             search
           </button>
