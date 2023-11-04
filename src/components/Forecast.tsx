e5bff2d7d3cc23905deb6220c9facfa77ab97df7
Kevin Masters <gmail@kevinmasters.com>
2023-11-03 21:09:10 -0400

    Componentize concerns (#4)
    * move functions to hook and search into its own component
    
    * create forecastType start working on forecast component logic
    
    * building out forecast component
    
    * further work on forecast widget
    
    * base weather info

---
+++ src/components/Forecast.tsx
@@ -0,0 +1,71 @@
+import { getSunTime } from '../helpers'
+import { forecastType } from '../types'
+import Sunrise from './Icons/Sunrise'
+import Sunset from './Icons/Sunset'
+
+type Props = {
+  data: forecastType
+}
+const Degree = ({ temp }: { temp: number }): JSX.Element => (
+  <span>{temp}&deg;</span>
+)
+const Forecast = ({ data }: Props): JSX.Element => {
+  const today = data.list[0]
+  return (
+    <div className="w-full md:max-w-[500px] py-4 md:py-4 md:px-10 lg:px-24 h-full lg:h-auto bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg">
+      <div className="mx-auto w-[300px]">
+        <section className="text-center">
+          <h2 className="text-2xl font-black">
+            {data.name}
+            <span className="font-thin"> {data.country}</span>
+          </h2>
+          <h1 className="text-4xl font-extrabold">
+            <Degree temp={Math.round(today.main.temp)} />
+          </h1>
+          <p className="text-sm">
+            {today.weather[0].main} {today.weather[0].description}
+          </p>
+          <p>
+            H: <Degree temp={Math.ceil(today.main.temp_max)} /> L:{' '}
+            <Degree temp={Math.floor(today.main.temp_min)} />
+          </p>
+        </section>
+        <section className="flex overflow-x-scroll mt-4 pb-2 mb-5">
+          {data.list.map((item, i) => (
+            <div
+              key={i}
+              className="inline-block text-center w-[50px] flex-shrink-0"
+            >
+              <p className="text-small">
+                {i === 0 ? 'Now' : new Date(item.dt * 1000).getHours()}
+                <img
+                  alt={`weather-icon-${item.weather[0].description}`}
+                  src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                />
+              </p>
+              <p className="text-small font-bold">
+                <Degree temp={Math.round(item.main.temp)} />
+              </p>
+            </div>
+          ))}
+        </section>
+        <section className="flex flex-wrap justify-between text-zinc-700">
+          <div className="w-[140px] text-xs font-bold flex flex-col items-center bg-white/20 backdrop-blur-ls rounded drop-shadow-lg py-4 mb-5">
+            <Sunrise /> <span className="mt-2">{getSunTime(data.sunrise)}</span>
+          </div>
+          <div className="w-[140px] text-xs font-bold flex flex-col items-center bg-white/20 backdrop-blur-ls rounded drop-shadow-lg py-4 mb-5">
+            <Sunset /> <span className="mt-2">{getSunTime(data.sunset)}</span>
+          </div>
+          {/* wind */}
+          {/* feels like */}
+          {/* humidity */}
+          {/* pop */}
+          {/* pressure */}
+          {/* visibility */}
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default Forecast
---
+++ src/hooks/useForecast.ts
@@ -0,0 +1,59 @@
+import { ChangeEvent, useEffect, useState } from "react"
+import { optionType, forecastType } from "../types"
+
+const useForecast = () => {
+    const [term, setTerm] = useState<string>('')
+    const [options, setOptions] = useState<[]>([])
+    const [city, setCity] = useState<optionType | null>(null)
+    const [forecast, setForecast] = useState<forecastType | null>(null)
+    const getSearchTermOptions = (value: string) => {
+        fetch(
+            `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${process.env.REACT_APP_API_KEY
+            }`
+        )
+            .then((res) => res.json())
+            .then((data) => setOptions(data))
+    }
+    const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value.trim()
+        setTerm(value)
+
+        if (value === '') return
+        getSearchTermOptions(value)
+    }
+
+    const onOptionSelect = (option: optionType) => {
+        setCity(option)
+    }
+
+    const getForecast = (city: optionType) => {
+        fetch(
+            `https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                const forecastData = {
+                    ...data.city,
+                    list: data.list.slice(0, 16),
+                }
+                setForecast(forecastData)
+            })
+    }
+    const onSubmit = () => {
+        if (!city) return
+
+        getForecast(city)
+    }
+    useEffect(() => {
+        if (city) {
+            setTerm(city.name)
+            setOptions([])
+        }
+    }, [city])
+
+    return {
+        term, options, forecast, onInputChange, onOptionSelect, onSubmit
+    }
+}
+
+export default useForecast
